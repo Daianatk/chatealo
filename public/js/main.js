a485e048b7f11707b70b613a25eefe319276158b
Daianatk <daiana.campos04gmail.com>
2020-09-16 14:55:50 -0500

    Message Console

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,39 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
 const socket = io();
 
-
+//Mensaje del servidor
 socket.on('message', message => {
-    console.log(message)
+    console.log(message);
+    outputMessage(message);
+
+    //Desplazarse hacia abajo Automaticamente
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+// Enviar mensaje
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Obtener el texto del mensaje
+    const msg = e.target.elements.msg.value;
+
+    //Emite mensaje al servidor
+    socket.emit('chatMessage' , msg);
+
+    //Limpiar input
+    e.target.elements.msg.value= '';
+    e.target.elements.msg.focus();
+});
+
+//Mensaje de Salida hacia el DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML= `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+    ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,11 +11,26 @@ const io = socketio(server);
 //Estableciendo carpeta estatica
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Chatealo Bot';
+
 //Se ejecuta cuando el cliente se conecta
 io.on('connection', socket => {
-    console.log('New WS Connection...');
 
-    socket.emit('message', 'Bienvenido a Chatealo!');
+    // Bienvenido para el usuario actual
+    socket.emit('message', formatMessage (botName, 'Bienvenido a Chatealo!'));
+
+    // Se ejecuta cuando un usuario se conecta
+    socket.broadcast.emit('message', formatMessage (botName,'A user has joined the chat'));
+
+    //Se ejecuta cuando el cliente se desconecta
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage (botName,'A user has left the chat'));
+    });
+
+    //Escucha el mensaje de chat
+    socket.on('chatMessage', msg => {
+        io.emit('message' , formatMessage ('USER', msg));
+    });
 });
 
 const PORT = process.env.PORT || 3000;
